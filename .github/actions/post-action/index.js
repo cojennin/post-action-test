cc690e0539e5d63de32472f2f5fb96f2ba3e94c9
Connor Jennings <cojennin@gmail.com>
2020-07-15 14:53:44 -0400

    Show me the PR

--- .github/actions/post-action/index.js
+++ .github/actions/post-action/index.js
@@ -1,10 +1,24 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
+const parsePullRequestId = githubRef => {
+  const result = /refs\/pull\/(\d+)\/merge/g.exec(githubRef);
+  if (!result) throw new Error("Reference not found.");
+  const [, pullRequestId] = result;
+  return pullRequestId;
+};
+
 try {
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.event, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  try {
+    const pullRequestId = parsePullRequestId(process.env.GITHUB_REF);
+    console.log(`The PR: ${pullRequestId}`)
+  } catch (e) {
+    console.log('No PR!');
+  }
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
+
 } catch (error) {
   core.setFailed(error.message);
 }
