26548056c36e88248b2aee8a4d9bf371f9d083a1
Connor Jennings <cojennin@gmail.com>
2020-07-15 14:56:36 -0400

    Fix this up

--- .github/actions/post-action/index.js
+++ .github/actions/post-action/index.js
@@ -12,7 +12,7 @@ try {
   // Get the JSON webhook payload for the event that triggered the workflow
   try {
     console.log(`The GH Ref: ${process.env.GITHUB_REF}`);
-    console.log(`The env: ${process.env}`);
+    console.log(`The env: ${JSON.stringify(process.env)}`);
     const pullRequestId = parsePullRequestId(process.env.GITHUB_REF);
     console.log(`The PR: ${pullRequestId}`)
   } catch (e) {
